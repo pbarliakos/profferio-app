282579f67ae55704ef12488dc84938eff6928101
unknown <pbarliakos@outlook.com>
2025-07-16 14:27:00 +0300

    Add user auth, role-based API and MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
 
 connectDB();
 
